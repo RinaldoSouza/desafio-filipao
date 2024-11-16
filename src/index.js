2a1530be95893494d30ba75f31e142c4f8ad4989
RinaldoSouza <rinaldopaulista.rsp@gmail.com>
2024-11-16 12:55:48 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
-let nome = "Rinaldo";
-console.log(nome);
+console.log("Desafio Classificador de Nível de Heroi");
+
+const rankingDeHerois = [
+  {
+    nome: "Sentinela",
+    pontos: 35000,
+  },
+  {
+    nome: "Spawn",
+    pontos: 9500,
+  },
+  {
+    nome: "Hulk",
+    pontos: 8980,
+  },
+  {
+    nome: "Deadpool",
+    pontos: 7501,
+  },
+  {
+    nome: "Senhor Destino",
+    pontos: 5560,
+  },
+  {
+    nome: "Doutor Manhattan",
+    pontos: 4780,
+  },
+  {
+    nome: "Doutor Estranho",
+    pontos: 1987,
+  },
+  {
+    nome: "Flash",
+    pontos: 870,
+  },
+];
+
+if (poder >= 1000) {
+    categoria = "Ferro";
+  } else if (poder >= 2000) {
+    categoria = "Bronze";
+  } else if (idade >= 5000) {
+    categoria = "Prata";  }
